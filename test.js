41a2de5a269490163a04704fae1e520abc21c6e3
discombobulateme <paloma.oliveira@saucelabs.com>
2020-10-28 19:33:31 +0100

    Cleam test file for isolated testing

--- test.js
+++ test.js
@@ -1,24 +1,22 @@
-const { Octokit } = require('@octokit/rest')
-const octokit = new Octokit()
-const repolinter = require('repolinter')
-const git = require('simple-git/promise')()
-
-// const repo = 'https://github.com/saucelabs/new-project.git'
-// git.clone(repo, './tmp')
+const { Octokit } = require('@octokit/rest') /* */
+const octokit = new Octokit() /*lib for GitHub API */
+const repolinter = require('repolinter') /*project which this is build upon */
+const git = require('simple-git/promise')() /*lib for GitHub API */
 
 async function main() {
+  // list public repository urls
   const { data } = await octokit.repos.listForOrg({
     org: 'saucelabs',
     type: 'public',
   })
 
+  // loop over the list of repository urls
   const urls = data.map(obj => obj.clone_url)
 
-  // execute repolinter default ruleset
   for (const url of urls.slice(0, 1)) {
-    // let urlToLint = await git.clone(url, './tmp')
+    // convert url in local repo
     await git.clone(url, './tmp')
-
+    // execute repolinter default ruleset
     const repolinterConnect = await repolinter.lint('./tmp')
     console.log(repolinterConnect.results)
 
@@ -27,6 +25,8 @@ async function main() {
       .filter(r => r.lintResult && !r.lintResult.passed)
       .map(r => r.lintResult.message)
     console.log(`In the repo ${url} there are a few missing things: ${results}\n`)
+    //     const results = lintResult.results.filter(r => r.ruleInfo.level === 'error').map(r => r.ruleInfo.name)
+    //     console.log(`In the repo ${url} there are a few missing things: ${results}\n`)
   }
 }
 
