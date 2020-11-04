12a58192e4cac93cb2771cedd84731f6be1b3a92
discombobulateme <paloma.oliveira@saucelabs.com>
2020-11-04 22:23:41 +0100

    Separate index2 for printing result in console

---
+++ src/index2.js
@@ -0,0 +1,67 @@
+// Copyright 2020 Sauce Labs. All rights reserved
+// Licensed under the MIT License
+
+const rainbowPenguin = require('rainbow-penguin')() /*lib for self.care while developing - delete when finish */
+
+const chalk = require('chalk')
+const log = console.log
+
+const { Octokit } = require('@octokit/rest') /* */
+const octokit = new Octokit() /*lib for GitHub API */
+const repolinter = require('repolinter') /*project which this is build upon */
+const git = require('simple-git/promise')() /*lib for GitHub API */
+const fs = require('fs')
+const os = require('os')
+const path = require('path')
+
+async function main() {
+  // list public repository urls
+  const { data } = await octokit.repos.listForOrg({
+    org: 'saucelabs',
+    type: 'public',
+  })
+
+  for (const d of data) {
+    let tmpDir = await fs.promises.mkdtemp(path.join(os.tmpdir(), 'repolinter-'))
+    await git.clone(d.clone_url, tmpDir)
+    const repolinterConnect = await repolinter.lint(tmpDir) /*execute repolinter default ruleset*/
+    const print = await repolinter.jsonFormatter.formatOutput(repolinterConnect) /*JS Object return into json*/
+    // fs.writeFileSync(path.resolve('./reports', `${Date()}-${d.name}`), print) /*creates json file*/
+
+    // filter messages for what didn't passed
+    const negResults = repolinterConnect.results
+      .filter(r => r.lintResult && !r.lintResult.passed)
+      .map(r => r.lintResult.targets)
+
+    const posResults = repolinterConnect.results.every(r => r.lintResult && r.lintResult.passed)
+
+    log(chalk`{blue Repository: ${d.name}}\n
+      {greenBright Passed: ${posResults}}\n
+      {red Failled: ${negResults}\n}`)
+
+    // log(chalk`{greenBright Repository: ${d.name}}\n
+    //   {blue Url: ${d.clone_url}}\n
+    //   Default message: ${results}\n`)
+  }
+}
+
+/**
+ * list of things
+ * - return with an error if validation fails for all results
+ * - create report
+ *     - print repository name and status of the linting
+ *     - Format: linterResults/saucelabs/monocle/20201029-111510/analysis.json
+                YYYYMMDD-HHmmSS
+ *     - if fails: list failures
+ * - also allow access to private repos
+ */
+
+// allows to be executed when not used as an imported file
+if (require.main === module) {
+  main().then(
+    () => console.log('Validation successful!'),
+    err => console.log('Validation failed:', err.stack)
+  )
+}
+
+module.exports = main
