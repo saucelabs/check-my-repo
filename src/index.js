4760c4ec8585061cedcadb53292a0d43643e5d7d
discombobulateme <paloma.oliveira@saucelabs.com>
2020-11-04 22:24:28 +0100

    Clean code to maintain clone repo and create json file

--- src/index.js
+++ src/index.js
@@ -27,29 +27,8 @@ async function main() {
     const repolinterConnect = await repolinter.lint(tmpDir) /*execute repolinter default ruleset*/
     const print = await repolinter.jsonFormatter.formatOutput(repolinterConnect) /*JS Object return into json*/
     fs.writeFileSync(path.resolve('./reports', `${Date()}-${d.name}`), print) /*creates json file*/
-
-    // filter messages for what didn't passed
-    const results = repolinterConnect.results
-      .filter(r => r.lintResult && !r.lintResult.passed)
-      .map(r => r.lintResult.message)
-
-    // log(chalk`{greenBright Repository: ${d.name}}\n
-    //   {blue Url: ${d.clone_url}}\n
-    //   {blackBright Temporary directory: ${tmpDir}}\n
-    //   Default message: ${results}\n`)
   }
 }
-
-/**
- * list of things
- * - return with an error if validation fails for all results
- * - create report
- *     - print repository name and status of the linting
- *     - Format: linterResults/saucelabs/monocle/20201029-111510/analysis.json
-                YYYYMMDD-HHmmSS
- *     - if fails: list failures
- * - also allow access to private repos
- */
 
 // allows to be executed when not used as an imported file
 if (require.main === module) {
