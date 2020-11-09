d1f3fa8d576b3ebb8eefeabf74049a1a62c74077
discombobulateme <paloma.oliveira@saucelabs.com>
2020-11-09 18:59:04 +0100

    Add vue project to create frontend

---
+++ frontend/src/App.vue
@@ -0,0 +1,26 @@
+<template>
+  <div id="app">
+    <Dashboard />
+  </div>
+</template>
+
+<script>
+import Dashboard from './components/Dashboard.vue'
+
+export default {
+  name: 'app',
+  components: {
+    Dashboard,
+  },
+}
+</script>
+
+<style lang="scss">
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+}
+</style>
