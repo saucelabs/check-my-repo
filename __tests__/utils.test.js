0a75e4f1e2794cd909af6013e20f9cf1ea35eda7
discombobulateme <paloma.oliveira@saucelabs.com>
2020-11-24 10:56:50 +0100

    Fix missing )} and change formatedDate test

--- __tests__/utils.test.js
+++ __tests__/utils.test.js
@@ -1,5 +1,4 @@
-const { createTempDirectory, printResults, date } = require('../src/utils')
-const { posResults, negResults } = require('../src/__mocks__')
+const { createTempDirectory, printResults, formatedDate } = require('../src/utils')
 const fs = require('fs')
 jest.mock('fs')
 
@@ -7,17 +6,15 @@ it("should create a new directory if one doesn't already exist", () => {
   // set up existsSync to meet the `if` condition
   fs.existsSync.mockReturnValue(false)
   createTempDirectory().then(path => {
-    expect(fs.mkdtemp).toHaveBeenCalled();
+    expect(fs.mkdtemp).toHaveBeenCalled()
+  })
 })
 
-it("should print a result nicely", () => {
-  expect(printResults())
-    .toBe(posResults, negResults);
-)
+it('should print a result nicely', () => {
+  expect(printResults()).toBe(posResults, negResults)
+})
 
-it("should create new date", () => {
-  jest
-    .spyOn(global, 'Date')
-    .mockImplementationOnce(() => new Date('2019-05-14T11:01:58.135Z'));
-  expect(date()).toEqual(new Date('2019-05-14T11'));
+it('should create new formated date', () => {
+  jest.spyOn(global, 'Date').mockImplementationOnce(() => new Date('2019-05-14T11:01:58.135Z'))
+  expect(formatedDate().toEqual(new Date('2019-05-14T11')))
 })
