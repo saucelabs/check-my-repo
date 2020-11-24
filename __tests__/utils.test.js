03088c87e9511f990c979818aa2206a20859c579
discombobulateme <paloma.oliveira@saucelabs.com>
2020-11-24 17:47:32 +0100

    Use TDD to test if real func is well written

--- __tests__/utils.test.js
+++ __tests__/utils.test.js
@@ -2,11 +2,14 @@ const { createTempDirectory, printResults, formatedDate } = require('../src/util
 const fs = require('fs')
 jest.mock('fs')
 
-it("should create a new directory if one doesn't already exist", () => {
-  // set up existsSync to meet the `if` condition
-  fs.existsSync.mockReturnValue(false)
-  createTempDirectory().then(path => {
-    expect(fs.mkdtemp).toHaveBeenCalled()
+// using TDD to verify if real function is well written
+describe('filtering data', () => {
+  it('should filter positive results', () => {
+    const posResults = Object.values(results)
+      .filter(r => r.status == 'passed')
+      .map(r => r.rule.name)
+
+    expect(posResults).toEqual(['Rule1', 'Rule5', 'Rule6'])
   })
 })
 
