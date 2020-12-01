4f0d9f861a3585dd14ed6b94d68b98cce724c669
discombobulateme <paloma.oliveira@saucelabs.com>
2020-12-01 15:19:12 +0100

    Add test to printResults function

--- __tests__/utils.test.js
+++ __tests__/utils.test.js
@@ -1,5 +1,10 @@
 const { printResults } = require('../src/utils')
 const results = require('./__fixtures__/results')
 
+test('should filter passed results', () => {
+  const data = { name: 'repository' }
+  const log = jest.fn()
+  printResults(data, results, log)
 
+  expect(log.mock.calls).toMatchSnapshot()
 })
