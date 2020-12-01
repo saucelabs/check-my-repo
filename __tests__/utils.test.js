37bea545ad2cd6538b897e0dee112454204f84dc
discombobulateme <paloma.oliveira@saucelabs.com>
2020-12-01 15:18:38 +0100

    Delete unecessary imports

--- __tests__/utils.test.js
+++ __tests__/utils.test.js
@@ -1,7 +1,4 @@
-const { createTempDirectory, formatedDate, printResults } = require('../src/utils')
+const { printResults } = require('../src/utils')
 
-const fs = require('fs')
-const { results, data } = require('../src/__mocks__/results')
-jest.mock('fs')
 
 })
