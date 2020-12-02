e9ae1beae183d51b7bccbbbc7d98ff4f0b3e13e5
discombobulateme <paloma.oliveira@saucelabs.com>
2020-12-02 18:12:15 +0100

    Add test to check is all results passed

--- __tests__/utils.test.js
+++ __tests__/utils.test.js
@@ -1,10 +1,19 @@
 const { printResults } = require('../src/utils')
 const results = require('./__fixtures__/results')
+const resultsPassed = require('./__fixtures__/results-passed')
 
 test('should filter passed results', () => {
   const data = { name: 'repository' }
   const log = jest.fn()
   printResults(data, results, log)
+
+  expect(log.mock.calls).toMatchSnapshot()
+})
+
+test('should return global all passed when there is no fail', () => {
+  const data = { name: 'repository' }
+  const log = jest.fn()
+  printResults(data, resultsPassed, log)
 
   expect(log.mock.calls).toMatchSnapshot()
 })
