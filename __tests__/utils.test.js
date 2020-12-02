0d4e57c825905a30f963b4c5df34a7a20aa59092
discombobulateme <paloma.oliveira@saucelabs.com>
2020-12-02 19:24:59 +0100

    Require fs and jest.mock('fs')

--- __tests__/utils.test.js
+++ __tests__/utils.test.js
@@ -2,6 +2,9 @@ const { printResults, createJsonFile } = require('../src/utils')
 const results = require('./__fixtures__/results')
 const resultsPassed = require('./__fixtures__/results-passed')
 
+const fs = require('fs')
+jest.mock('fs')
+
 test('should filter passed results', () => {
   const data = { name: 'repository' }
   const log = jest.fn()
