c638bf6b4c21e06e87f680d0493f0c49625772e4
discombobulateme <paloma.oliveira@saucelabs.com>
2020-12-03 16:17:34 +0100

    Add jsonFormatter mock returning a JSON parsed

--- __tests__/__mocks__/repolinter.js
+++ __tests__/__mocks__/repolinter.js
@@ -3,6 +3,8 @@ const { results } = require('../__fixtures__/results')
 const repolinter = {
   runRuleset: true,
   lint: jest.fn().mockReturnValue(Promise.resolve(results)),
+  jsonFormatter: {
+    formatOutput: jest.fn().mockReturnValue(Promise.resolve(JSON.stringify({ hello: 1 }))),
+  },
 }
-
 module.exports = repolinter
