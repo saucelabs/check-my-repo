db93deb4364832b46db0eba42e5887f8fa11544f
discombobulateme <paloma.oliveira@saucelabs.com>
2020-12-15 15:44:21 +0100

    Delete CheckedRepositories func to get data from length

--- src/utils.js
+++ src/utils.js
@@ -96,11 +96,4 @@ const validateChangeLog = async function (results, organization, repository) {
   }
 }
 
-/* Returns a sum of the repositories analized */
-const sumCheckedRepositories = async function (data) {
-  let sum = 0
-  data.forEach(d => sum++)
-  console.log(chalk`\nNumber of repositories analised: {cyanBright.bold ${sum}}\n`)
-}
-
-module.exports = { printResults, createJsonFile, formatedDate, validateChangeLog, sumCheckedRepositories }
+module.exports = { printResults, createJsonFile, formatedDate, validateChangeLog }
