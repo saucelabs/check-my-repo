f936137af06db91ea302b3cd1f9adb25a677b618
discombobulateme <paloma.oliveira@saucelabs.com>
2020-12-29 19:43:47 +0100

    Add CSS attributes

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -17,10 +17,12 @@ export default {
 
 <style lang="scss">
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
-  color: #2c3e50;
+  display: flex;
+  align-items: center;
+  justify-content: center;
 }
 </style>
