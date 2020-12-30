2d31ec94a368573de6e91f4d7db1c7620d9daabb
Paloma Oliveira <paloma.oliveira@saucelabs.com>
2020-12-30 23:15:23 +0100

    Dashboard (#65)
    * Delete About component and route
    
    * Add v-for biding to show filtered data
    
    * Add scoped style to button
    
    * Change data file
    
    * Add comment to explain data ()
    
    * Add axios
    
    * Change method from fetch to axios
    
    * Test getting data from simpler json structure
    
    * Add computed method to get sum of analised repos
    
    * Add computed method to sum all repos that passed
    
    * Add catch error to methods axios get request
    
    * Add computed method to check how many failures was found
    
    * Add axios with results filter
    
    * Add display after clicking button with all returned data
    
    * Add computed method
    
    * Add CSS attributes
    
    * Add circle style to button
    
    * Add square style around each response
    
    * Add content style
    
    * Add styles to each response
    
    * Change style to have responses wrapped around squares
    
    * Add bold style to results
    
    * Add vue config file to add vue debugger
    
    * Delete all data return
    
    * Allow responses be trigger by button
    
    * Delete returns
    
    * Add map and filter to return
    
    * Add filter to check all non passed results, working
    
    * Add filter to check if all results passed
    
    * Testing filters
    
    * Correct all passed method
    
    * Add Sass
    
    * Change default font to Open Sans
    
    * Add base style
    
    * Simplify response text
    
    * Add on hover to enhace result on hover
    
    * Change square color to white
    
    * Add Scss
    
    * Add borders

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -17,10 +17,13 @@ export default {
 
 <style lang="scss">
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
+  font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans',
+    'Helvetica Neue', sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
-  color: #2c3e50;
+  display: flex;
+  align-items: center;
+  justify-content: center;
 }
 </style>
--- frontend/src/components/Dashboard.vue
+++ frontend/src/components/Dashboard.vue
@@ -1,25 +1,123 @@
 <template>
   <div>
-    <h1>OSPO Verified Repositories</h1>
-    <button v-on:click="getDashboardData">Get Dashboard Data</button>
-    <div>{{ dashboardDataList }}</div>
+    <h1>CHECK MY REPO</h1>
+    <button v-on:click="getDashboardData">Check</button>
+    <div class="square">
+      <div class="content total">
+        Repositories
+        <div class="result">{{ reposAnalised }}</div>
+      </div>
+      <div class="content passed">
+        All Passed
+        <div class="result">{{ allPassed }}</div>
+      </div>
+      <div class="content failed">
+        With Failures
+        <div class="result">{{ failedRepo }}</div>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
+import axios from 'axios'
+
 export default {
   name: 'Dashboard',
+  /* tells Vue.js that we want to add these variables to our reactivity system */
   data() {
     return {
       dashboardDataList: [] /* stored in public folder */,
+      errored: false,
     }
   },
   methods: {
     getDashboardData() {
-      fetch('dashboard.json')
-        .then(response => response.json())
-        .then(data => (this.dashboardDataList = data))
+      axios
+        .get('dashboard.json')
+        .then(response => (this.dashboardDataList = response.data.results))
+        .catch(error => {
+          console.log(error)
+          this.errored = true
+        })
+    },
+  },
+  computed: {
+    allPassed: function() {
+      let allPassed = 0
+      let hasFailures = this.dashboardDataList.filter(r => !r.lintResult.passed).length > 0
+      return !hasFailures ? allPassed++ : 0
+    },
+    failedRepo: function() {
+      return this.dashboardDataList.filter(r => r.lintResult && !r.lintResult.passed).length
+    },
+    reposAnalised: function() {
+      return this.dashboardDataList.length
     },
   },
 }
 </script>
+
+<style scoped lang="scss">
+button {
+  width: 100px;
+  height: 100px;
+  line-height: 50px;
+  border-radius: 50%;
+  font-size: 30px;
+  color: #fff;
+  text-align: center;
+  background: #003a70;
+}
+.background {
+  background-color: #ededef;
+  font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans',
+    'Helvetica Neue', sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+}
+
+.square {
+  width: 80vw;
+  height: auto;
+  display: flex;
+  background-color: white;
+}
+.content {
+  font-size: 1.5em;
+  padding: 5%;
+  margin: 2%;
+}
+.total {
+  color: #464b54;
+  border-radius: 10px;
+  border: 3px solid #464b54;
+  &:hover {
+    border: 6px solid #464b54;
+  }
+}
+.passed {
+  color: #003a70;
+  border-radius: 10px;
+  border: 3px solid #003a70;
+  &:hover {
+    border: 6px solid #003a70;
+  }
+}
+.failed {
+  color: #e12726;
+  border-radius: 10px;
+  border: 3px solid #e12726;
+  &:hover {
+    border: 6px solid #e12726;
+  }
+}
+.result {
+  font-weight: 500;
+  font-size: 2em;
+}
+</style>
