c6ed94405a8cabfdb35ba47c03a312b1c1396701
discombobulateme <paloma.oliveira@saucelabs.com>
2020-12-30 23:08:44 +0100

    Change default font to Open Sans

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -17,7 +17,8 @@ export default {
 
 <style lang="scss">
 #app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans',
+    'Helvetica Neue', sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
