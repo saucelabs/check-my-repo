3e313d0d0562d07b5e63baf989e7f0ee39e9f23a
discombobulateme <paloma.oliveira@saucelabs.com>
2021-01-09 21:33:39 +0100

    Add About component with bor image

---
+++ frontend/src/components/About.vue
@@ -0,0 +1,26 @@
+<template>
+  <div>
+    <svg :view-box="ospo-bot"></svg>
+
+    <p>About</p>
+    <p>Open source automated tool daily checking our public
+      repositories health and quality
+    </p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'About'
+}
+</script>
+
+<style scoped lang="scss">
+p {
+  text-align: center;
+  font-size: 1em;
+  color: white;
+  font-weight: 200;
+}
+
+</style>
