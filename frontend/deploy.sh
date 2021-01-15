970d07f0074e300d1bbd112c1276f05b6676dee6
discombobulateme <paloma.oliveira@saucelabs.com>
2021-01-15 17:14:00 +0100

    Fix script deploy

--- frontend/deploy.sh
+++ frontend/deploy.sh
@@ -8,6 +8,17 @@ cd dist
 # if you are deploying to a custom domain
 # echo 'www.example.com' > CNAME
 
+if [ -n "$GITHUB_TOKEN" ]
+then
+  echo -e "\033[0;32mDeploying site via GitHub Actions...\033[0m"
+  touch ~/.git-credentials
+  chmod 0600 ~/.git-credentials
+  echo $GITHUB_TOKEN > ~/.git-credentials
+  git config credential.helper store
+  git config user.email "oss-sauce-bot@users.noreply.github.com"
+  git config user.name "OSS Sauce Bot"
+fi
+
 git init
 git add -A
 git commit -m 'deploy'
