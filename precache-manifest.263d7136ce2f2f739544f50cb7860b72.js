cec9bcecb1f6b2cfdcc95ae8778d41d74b1c048e
discombobulateme <paloma.oliveira@saucelabs.com>
2021-01-15 23:23:52 +0100

    deploy

---
+++ precache-manifest.263d7136ce2f2f739544f50cb7860b72.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "19e32ec4a5f7715fd3bf",
+    "url": "/check-my-repo/css/app.9bc67cad.css"
+  },
+  {
+    "revision": "aa6c2d456c0a70e0ea8396994cd9086b",
+    "url": "/check-my-repo/frontend.json"
+  },
+  {
+    "revision": "82b9c7a5a3f405032b1db71a25f67021",
+    "url": "/check-my-repo/img/logo.82b9c7a5.png"
+  },
+  {
+    "revision": "ee94a6e79e1242696409eb81a29c2344",
+    "url": "/check-my-repo/img/ospo-bot.ee94a6e7.svg"
+  },
+  {
+    "revision": "7c418a09dd505bc68c50f085cca5457c",
+    "url": "/check-my-repo/index.html"
+  },
+  {
+    "revision": "19e32ec4a5f7715fd3bf",
+    "url": "/check-my-repo/js/app.eae66490.js"
+  },
+  {
+    "revision": "04bf2d768c85fe2e2fbc",
+    "url": "/check-my-repo/js/chunk-vendors.8d1effd9.js"
+  },
+  {
+    "revision": "4b14c64efaf846819b9a229b4193c8b7",
+    "url": "/check-my-repo/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/check-my-repo/robots.txt"
+  }
+]);
