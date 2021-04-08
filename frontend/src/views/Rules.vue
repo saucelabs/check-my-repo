ae0d35c71431551e7f58e093ff181f0662164686
discombobulateme <paloma.oliveira@saucelabs.com>
2021-04-08 22:24:23 +0200

    Add Rules View

---
+++ frontend/src/views/Rules.vue
@@ -0,0 +1,46 @@
+<template>
+  <div>
+    <h1>Rules</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  components: {  },
+  name: 'Rules'
+}
+</script>
+
+<style scoped lang="scss">
+p {
+  text-align: center;
+  font-size: 1em;
+  color: white;
+  font-weight: 200;
+}
+.p-bottom {
+  padding-bottom: 10%;
+}
+
+.break-line {
+  margin: 0%;
+}
+
+.bot {
+  margin-top: 5%;
+  display: block;
+  margin-left: auto;
+  margin-right: auto;
+  width: 6%;
+}
+
+a {
+  display: block;
+  text-align: center;
+  margin: 2%;
+  font-size: 1em;
+  color: white;
+  font-weight: 200;
+}
+
+</style>
