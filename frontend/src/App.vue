5562bc720d3b4c87d29093ddbd4ff38215c2cf4c
discombobulateme <paloma.oliveira@saucelabs.com>
2021-04-16 15:39:10 +0200

    Add badge css to insert badges in rontend results

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -33,4 +33,52 @@ body {
   border: 0vh;
 }
 
+.badge {
+  background: var(--color-prime-light-grey);
+  border: none;
+  border-radius: var(--border-radius);
+  color: var(--color-prime-olive);
+  font-size: var(--font-size-xs);
+  filter: drop-shadow(2px 2px 12px rgba(0, 0, 0, 0));
+  text-align: center;
+  overflow: hidden;
+  transition: background-color var(--transition-time), filter var(--transition-time);
+  text-decoration: none;
+}
+
+@media screen and (min-width: 768px) {
+  .badge {
+    font-size: var(--font-size-large);
+  }
+}
+
+.badge:hover {
+  background: var(--color-prime-light-grey-hover);
+  /* filter: drop-shadow(2px 2px 12px rgba(0, 0, 0, 0.25)); */
+}
+
+.badge--shadow {
+  filter: drop-shadow(2px 2px 12px rgba(0, 0, 0, 0.25));
+}
+
+.badge--shadow:hover {
+  filter: drop-shadow(2px 2px 25px rgba(0, 0, 0, 0.3));
+}
+
+.badge--green {
+  background-color: var(--color-prime-green);
+}
+
+.badge--green:hover {
+  background-color: var(--color-prime-green-hover);
+}
+
+.badge--pink {
+  background-color: var(--color-prime-pink);
+}
+
+.badge--pink:hover {
+  background-color: var(--color-prime-pink-hover);
+}
+
 </style>
