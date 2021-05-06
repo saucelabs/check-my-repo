55393294c60ada624d6bc84162481e385f7f2ca2
Paloma Oliveira <paloma.oliveira@saucelabs.com>
2021-05-07 00:04:19 +0200

    Plug n play (#147)
    * Delete unnecessary config files
    
    * Change logo name to a neutral logo to be easily adapted
    
    * Add vue-axe to check accessibility
    
    * Change variable to logo name
    
    * Add vue-axe in dev env
    
    * Change variable from config to frontend.js
    
    * Change variables to env variables to be used with GH actions
    
    * Add validation to throw error if repo was not found
    
    * Add url info to be added in frontend
    
    * Add worksflow to create artifact
    
    * Delete branch specificity
    
    * Add updated json file from gh actions artifact
    
    * Change variable
    
    * Try new variable to get owner url
    
    * Update json file
    
    * Get repo url infro from frontend.json file

--- src/index.js
+++ src/index.js
@@ -4,7 +4,13 @@ const repolinter = require('repolinter') /*project which this is build upon */
 const git = require('simple-git/promise')() /*lib for GitHub API */
 const chalk = require('chalk')
 
-const myVariables = require('../check-my-repo-config.json')
+const repository = process.env.GITHUB_REPOSITORY
+
+if (repository === null) {
+  throw 'This GitHub user apparently does not exist';
+}
+
+const [owner] = repository.split('/')
 
 const path = require('path')
 const fs = require('fs')
@@ -18,15 +24,13 @@ const {
   createJsonDashboardFile,
 } = require('./utils')
 
-const input = myVariables.owner
-
 /* This variable stores the sum of all analised repositories which results are all positives */
 let passingRepositories = 0
 
 async function main() {
 
   /* Verifies if it is an organization or a user */
-  const { data: { type } } = await octokit.request(`GET /users/${input}`)
+  const { data: { type } } = await octokit.request(`GET /users/${owner}`)
 
   const fetchRepos = type === 'Organization' ? octokit.repos.listForOrg : octokit.repos.listForUser
 
@@ -34,10 +38,11 @@ async function main() {
   const results = []
   /* Parameters to call octokit API requests */
   const parameters = {
-      org: input,
-      username: input,
-      per_page: myVariables.pagination
-    }
+      org: owner,
+      username: owner,
+      per_page: 100,
+  }
+
   /* This function allows to iterate over all paginations, as explained in documentaton */
   for await (const response of octokit.paginate.iterator(fetchRepos, parameters))
     {
@@ -53,7 +58,7 @@ async function main() {
     const repolinterConnect = await repolinter.lint(tmpDir) /*execute repolinter default ruleset*/
 
     /* Validates if Changelog rule passed, of not, search for releases */
-    await validateChangeLog(repolinterConnect.results, input, d.name)
+    await validateChangeLog(repolinterConnect.results, owner, d.name)
 
     /* Print in all the results in terminal */
     printResults(d, repolinterConnect.results)
@@ -68,6 +73,7 @@ async function main() {
 
     /* Push individual repos results to the array which will contain all the results */
     output.push({
+      repo: d.owner.html_url,
       name: d.name,
       url: d.clone_url,
       failed: negativeResults(repolinterConnect.results),
